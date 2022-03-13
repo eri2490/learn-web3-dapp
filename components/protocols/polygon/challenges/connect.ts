a2eb92052bc3ee2aa3511b83eee29644354734b4
eri2490 <e.147xxx@gmail.com>
2022-03-13 14:21:02 +0900

    feat: connect metamask

--- components/protocols/polygon/challenges/connect.ts
+++ components/protocols/polygon/challenges/connect.ts
@@ -6,11 +6,19 @@ declare let window: {
 
 const connect = async () => {
   try {
-    const provider = undefined;
+    // Define the provider by calling `web3Provider` method of `providers`
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     if (provider) {
-      await undefined;
-      const signer = undefined;
-      const address = undefined;
+      // ensure that Metamask connects to the page
+      // and that we can query the currently selected account in Metamask.
+      // this is done by using `send` on the provider, to send the `eth_requestAccounts`
+      await provider.send('eth_requestAccounts', []);
+      // bring up  a Metamask dialog, asking the user to unlock their Metamask if it is locked,
+      // or to connect an account to the page if Metamask is unlocked
+      const signer = provider.getSigner();
+      // now signer represents the current connected account.
+      // `getAddress` method will do the job of querying the address of the current connected account
+      const address = await signer.getAddress();
       return {
         address,
       };

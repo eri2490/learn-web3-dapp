911847684e50549e2a464ba940488facea0feba2
eri2490 <e.147xxx@gmail.com>
2022-03-13 14:38:07 +0900

    feat: query Polygon and retrieve information from the blockchain

--- components/protocols/polygon/challenges/query.ts
+++ components/protocols/polygon/challenges/query.ts
@@ -7,16 +7,26 @@ declare let window: {
 const query = async () => {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    /* `networkName` await `provider.getNetwork()` */
     const networkName = await provider.getNetwork().then((res) => {
       return res.name;
     });
 
-    // TODO: Define the variables below
-    const chainId = undefined;
-    const blockHeight = undefined;
-    const gasPriceAsGwei = undefined;
-    const blockInfo = undefined;
-
+    // TODO: create a way to query information from the blockchain and then display it on the UI
+    /* we can get `chainId` as a property of `provider.network` */
+    const {chainId} = await provider.getNetwork();
+    /* `blockHeight` can be taken directly from the returned values of `getBlockNumber` */
+    const blockHeight = await provider.getBlockNumber();
+    /* `gasPriceAsGwei` get the current gas price and then formats the value into a human-friendly 
+      number with the ethers utility function `formatUnits`
+    */
+    const gasPriceAsGwei = await provider.getGasPrice().then((res) => {
+      return ethers.utils.formatUnits(res);
+    });
+    /* `blockInfo` must be a BlockWithTransactions type, 
+      which is what function `getBlockWithTransactions` */
+    const blockInfo = await provider.getBlockWithTransactions(blockHeight);
     if (!chainId || !blockHeight || !gasPriceAsGwei || !blockInfo) {
       throw new Error('Please complete the code');
     }

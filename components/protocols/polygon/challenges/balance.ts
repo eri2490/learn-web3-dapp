0edc3a54b649033532ce36fed87cd490a4254d92
eri2490 <e.147xxx@gmail.com>
2022-03-13 14:45:47 +0900

    feat: fund a Polygon account

--- components/protocols/polygon/challenges/balance.ts
+++ components/protocols/polygon/challenges/balance.ts
@@ -8,11 +8,16 @@ const getBalance = async (address: string) => {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    const balance = undefined;
+    // Todo: show the wallet balance
+    /* Promise returns a BigNumber, which is a specific data type for handling numbers
+      which fall outside the range of safe values in JS
+    */
+    const balance = await provider.getBalance(address);
     if (!balance) {
       throw new Error('Please complete the code');
     }
     return {
+      /* to easy manipulate the returned type we convert it to string using `toString()` */
       balance: balance.toString(),
     };
   } catch (error) {

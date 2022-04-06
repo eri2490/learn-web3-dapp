8aada1fb43698195e95d91ecb5116f049e6dfc11
eri2490 <e.147xxx@gmail.com>
2022-04-07 08:27:08 +0900

    feat: deploy testnet

--- contracts/polygon/SimpleStorage/contracts/SimpleStorage.sol
+++ contracts/polygon/SimpleStorage/contracts/SimpleStorage.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint storedData;
 
+    // The constructor() is called only once, when the contract is deployed.
+    // is the place to assign default values
     constructor() {
   		  storedData = 0;
   	}

68e5b9316c76b69054772325c80c589ccfffa5a8
eri2490 <e.147xxx@gmail.com>
2022-04-07 08:38:23 +0900

    feat: interact with contract

--- components/protocols/polygon/challenges/setter.ts
+++ components/protocols/polygon/challenges/setter.ts
@@ -9,10 +9,13 @@ const setValue = async (contractAddress: string, value: number) => {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    // try to figure out the expected parameters
-    const contract = new ethers.Contract(undefined);
-    // try to figure out the expected method
-    const transactionResult = undefined;
+    const contract = new ethers.Contract(
+      contractAddress,
+      SimpleStorageJson.abi,
+      signer,
+    );
+    // contract.METHOD_NAME() で、コントラストに定義した関数が使える
+    const transactionResult = await contract.set(value);
     const receipt = await transactionResult.wait();
     return {hash: receipt.transactionHash};
   } catch (error) {

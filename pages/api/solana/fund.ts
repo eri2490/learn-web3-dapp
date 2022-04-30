9f8e98f08cb40aa9fdb428b62c563a262ff952ce
eri2490 <e.147xxx@gmail.com>
2022-04-30 19:49:23 +0900

    feat: get 1 sol

--- pages/api/solana/fund.ts
+++ pages/api/solana/fund.ts
@@ -7,12 +7,15 @@ export default async function fund(
   res: NextApiResponse<string>,
 ) {
   try {
+    // get 1 SOL as request Airdrop
+    // solscan https://explorer.solana.com/tx/3tsvtHdbfS9aiqfh4xbzQam4GHMiL1Nn1Kjq5x4g62sDiU7WK7cQYPvnjSGSv19FogyfpheeAj1SeL7qqkmrbt24?cluster=devnet
     const {network, address} = req.body;
     const url = getNodeURL(network);
     const connection = new Connection(url, 'confirmed');
-    const publicKey = undefined;
-    const hash = undefined;
-    await undefined;
+    const publicKey = new PublicKey(address);
+    const hash = await connection.requestAirdrop(publicKey, LAMPORTS_PER_SOL);
+    console.log(hash);
+    await connection.confirmTransaction(hash);
     res.status(200).json(hash);
   } catch (error) {
     let errorMessage = error instanceof Error ? error.message : 'Unknown Error';

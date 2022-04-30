3d5342e7b70d34a95cba1424fc75af3734bdb9f0
eri2490 <e.147xxx@gmail.com>
2022-04-30 20:15:39 +0900

    feat: transfer from signer to a account

--- pages/api/solana/transfer.ts
+++ pages/api/solana/transfer.ts
@@ -12,6 +12,11 @@ export default async function transfer(
   req: NextApiRequest,
   res: NextApiResponse<string>,
 ) {
+  // to transfer some value to another account, we have to create and send a signed transaction to the `cluster`
+  // when a transaction is submitted to the `cluster` in the transaction,
+  // the solana runtime will execute a program to process each of the instructions contained in the transaction
+  // if any of the instructions fail for any reason, the entire transaction will revert
+
   try {
     const {address, secret, recipient, lamports, network} = req.body;
     const url = getNodeURL(network);
@@ -22,21 +27,35 @@ export default async function transfer(
     // The secret key is stored in our state as a stringified array
     const secretKey = Uint8Array.from(JSON.parse(secret as string));
 
-    //... let's skip the beginning as it should be familiar for you by now!
-    // Find the parameter to pass
-    const instructions = SystemProgram.transfer;
+    // supply a `sender`, a `receiver`, an `amount`
+    const instructions = SystemProgram.transfer({
+      fromPubkey,
+      toPubkey,
+      lamports,
+    });
 
-    // How could you construct a signer array's
-    const signers = undefined;
+    // signers Array contains both the signers `publicKey` & `secretKey`
+    // the `secretKey` is used to sign the transaction
+    const signers = [
+      {
+        publicKey: fromPubkey,
+        secretKey,
+      },
+    ];
 
-    // Maybe adding something to a Transaction could be interesting ?
-    const transaction = new Transaction();
+    // create new `Transaction` object add the instructions to it
+    const transaction = new Transaction().add(instructions);
 
-    // We can send and confirm a transaction in one row.
-    const hash = undefined;
+    // await the confirmation of the signed transaction using `sendAndConfirmTransaction`
+    const hash = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      signers,
+    );
 
     res.status(200).json(hash);
   } catch (error) {
+    console.log(error);
     let errorMessage = error instanceof Error ? error.message : 'Unknown Error';
     res.status(500).json(errorMessage);
   }
